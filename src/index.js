92df24f40a8055943270336c6ba2d28f5f5c1772
Krzysztof Jan Modras <chrmod@chrmod.net>
2017-05-18 21:27:51 +0200

    super basic test setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import Spanan from "spanan";
+
+const coreWrapper = new Spanan((message) => {
+  postMessage(JSON.stringify({
+    module: 'core',
+    action: message.functionName,
+    args: message.args,
+    requestId: message.udid,
+  }));
+});
+
+const core = coreWrapper.createProxy();
+
+core.init();
--- tests/browser/index.js
+++ tests/browser/index.js
@@ -1,4 +1,17 @@
-describe("test", function () {
-  it("test", function () {
+import { expect } from "chai";
+
+describe("Worker", function () {
+  it("sents init message to core", function (done) {
+    const worker = new Worker("/dist/index.browser.js");
+    worker.addEventListener("message", function (ev) {
+      const message = JSON.parse(ev.data);
+      try {
+        expect(message).to.have.property('module').that.equal('core');
+        expect(message).to.have.property('action').that.equal('init');
+        done();
+      } catch(e) {
+        done(e);
+      }
+    });
   });
 })
