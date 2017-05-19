0cc7f9c69bfd7c6a632d09a089bb8d0a8c53edae
Krzysztof Jan Modras <chrmod@chrmod.net>
2017-05-19 22:14:48 +0200

    Testing communication in both ways

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import Spanan from "spanan";
 
 const coreWrapper = new Spanan((message) => {
-  postMessage(JSON.stringify({
+  postMessage({
     module: 'core',
     action: message.functionName,
     args: message.args,
     requestId: message.udid,
-  }));
+  });
 });
 
 const core = coreWrapper.createProxy();
 
 core.init();
+
+onmessage = function (ev) {
+  postMessage({
+    response: ev.data,
+  });
+}
--- tests/browser/index.js
+++ tests/browser/index.js
@@ -1,14 +1,34 @@
 import { expect } from "chai";
 
 describe("Worker", function () {
+  let worker;
+
+  beforeEach(function () {
+    worker = new Worker("/dist/index.browser.js");
+  });
+
   it("sents init message to core", function (done) {
-    const worker = new Worker("/dist/index.browser.js");
     worker.addEventListener("message", function (ev) {
-      const message = JSON.parse(ev.data);
+      const message = ev.data;
       try {
         expect(message).to.have.property('module').that.equal('core');
         expect(message).to.have.property('action').that.equal('init');
         done();
+      } catch(e) {
+        done(e);
+      }
+    });
+  });
+
+  it("respond to echo call", function (done) {
+    const testMessage = "some message";
+    worker.addEventListener("message", function (ev) {
+      worker.postMessage(testMessage);
+      try {
+        const message = ev.data;
+        if (message.response === testMessage) {
+          done();
+        }
       } catch(e) {
         done(e);
       }
